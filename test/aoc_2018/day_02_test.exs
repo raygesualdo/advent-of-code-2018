53fbbacdf40f2ed14f20cb99666bc5d97c3eb68c
Ray Gesualdo <ray@raygesualdo.com>
2018-12-03 17:47:16 -0500

    day 2, part 1

---
+++ test/aoc_2018/day_02_test.exs
@@ -0,0 +1,14 @@
+defmodule Aoc2018.Day02Test do
+  use ExUnit.Case, async: true
+
+  alias Aoc2018.Day02.Part1
+  # alias Aoc2018.Day02.Part2
+
+  test "solves part 1" do
+    assert Part1.solve("abcdef bababc abbcde abcccd aabcdd abcdee ababab") == 12
+  end
+
+  test "Part1.group_by_count" do
+    assert Part1.group_by_count(["a", "a", "b"]) == %{"a" => 2, "b" => 1}
+  end
+end
