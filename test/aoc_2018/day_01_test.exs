cae918493498803df429d553c56f4d78f239be94
Ray Gesualdo <ray@raygesualdo.com>
2018-12-03 17:46:49 -0500

    day 1, parts 1 & 2

---
+++ test/aoc_2018/day_01_test.exs
@@ -0,0 +1,18 @@
+defmodule Aoc2018.Day01Test do
+  use ExUnit.Case, async: true
+
+  alias Aoc2018.Day01.Part1
+  alias Aoc2018.Day01.Part2
+
+  test "solves the first problem" do
+    assert Part1.solve("+1 +10 -3") == 8
+    assert Part1.solve("+1 +1 -2") == 0
+    assert Part1.solve("-1 -2 -3") == -6
+  end
+
+  test "solves the second problem" do
+    assert Part2.solve("+1 -1") == 0
+    assert Part2.solve("+3 +3 +4 -2 -4") == 10
+    assert Part2.solve("-6 +3 +8 +5 -6") == 5
+  end
+end
