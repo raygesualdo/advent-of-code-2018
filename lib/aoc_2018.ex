ebdd3f094648f99e2b3020d17217905e915d7db0
Ray Gesualdo <ray@raygesualdo.com>
2018-12-03 17:54:27 -0500

    added mix task and input helpers

--- lib/aoc_2018.ex
+++ lib/aoc_2018.ex
@@ -1,2 +1,11 @@
 defmodule Aoc2018 do
+  def read_file(day) do
+    File.read!("data/day#{day}.txt")
+    |> String.split()
+    |> Enum.map(&String.trim/1)
+  end
+
+  def input(day) do
+    read_file(day)
+  end
 end
---
+++ lib/mix/tasks/solve.ex
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.Solve do
+  use Mix.Task
+
+  def run([day, part]) do
+    day = to_string(day) |> String.pad_leading(2, "0")
+    part = to_string(part)
+
+    result =
+      with_input(day, fn input ->
+        "Elixir.Aoc2018.Day#{day}.Part#{part}"
+        |> String.to_existing_atom()
+        |> apply(:solve, [input])
+      end)
+
+    Mix.shell().info("Day #{day}, Part #{part}: #{result}")
+  end
+
+  def run([day]) do
+    Enum.each([1, 2], &run([day, &1]))
+  end
+
+  def run([]) do
+    Enum.each(1..25, &run([&1]))
+  end
+
+  defp with_input(day, fun) do
+    Aoc2018.input(day)
+    |> fun.()
+  rescue
+    File.Error -> "No Input"
+  end
+end
