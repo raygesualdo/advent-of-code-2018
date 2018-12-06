b5098def7782f21324300bc6d7ef62822be8b776
Ray Gesualdo <ray@raygesualdo.com>
2018-12-06 09:36:06 -0500

    day 6, part 2

--- test/aoc_2018/day_06_test.exs
+++ test/aoc_2018/day_06_test.exs
@@ -2,7 +2,7 @@ defmodule Day06Test do
   use ExUnit.Case, async: true
 
   alias Day06.Part1
-  # alias Day06.Part2
+  alias Day06.Part2
 
   test "solves part 1" do
     input = "
@@ -16,7 +16,15 @@ defmodule Day06Test do
     assert Part1.solve(String.split(input, "\n", trim: true)) == 17
   end
 
-  # test "solves part 2" do
-  #   assert Part2.solve(String.split("")) == :ok
-  # end
+  test "solves part 2" do
+    input = "
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9
+"
+    assert Part2.solve(String.split(input, "\n", trim: true)) == 16
+  end
 end
