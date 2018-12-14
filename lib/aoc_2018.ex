4032c3b0382b0f07b75dd7044e1273e59ca617e4
Ray Gesualdo <ray@raygesualdo.com>
2018-12-13 22:30:11 -0500

    removed string trimming from input processing pipeline

--- lib/aoc_2018.ex
+++ lib/aoc_2018.ex
@@ -2,7 +2,8 @@ defmodule Aoc2018 do
   def read_file(day) do
     File.read!("data/day#{day}.txt")
     |> String.split(~r/\R/, trim: true)
-    |> Enum.map(&String.trim/1)
+
+    # |> Enum.map(&String.trim/1)
   end
 
   def input(day) do
